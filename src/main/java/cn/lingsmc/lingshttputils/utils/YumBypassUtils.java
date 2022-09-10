e2192153edc152e9a4df570b5901c0fbac2eb051
泠辰 <2154818001@qq.com>
2022-09-10 21:01:00 +0800

    屏蔽Yum网络监控刷屏

---
+++ src/main/java/cn/lingsmc/lingshttputils/utils/YumBypassUtils.java
@@ -0,0 +1,22 @@
+package cn.lingsmc.lingshttputils.utils;
+
+import cn.lingsmc.lingshttputils.LingsHTTPUtils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+import pw.yumc.Yum.events.PluginNetworkEvent;
+
+/**
+ * @author Crsuh2er0
+ * &#064;date 2022/9/10
+ * @apiNote
+ */
+public class YumBypassUtils implements Listener {
+
+    @EventHandler
+    public void yumBypass(@NotNull PluginNetworkEvent e){
+        if(e.getPlugin() == LingsHTTPUtils.getInstance()){
+            e.setCancelled(true);
+        }
+    }
+}
