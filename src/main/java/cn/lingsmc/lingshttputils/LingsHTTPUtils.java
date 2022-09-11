9511d698942974631bbf77eb7178e779bf8cae58
泠辰 <2154818001@qq.com>
2022-09-11 14:09:54 +0800

    修复Yum绕过

--- src/main/java/cn/lingsmc/lingshttputils/LingsHTTPUtils.java
+++ src/main/java/cn/lingsmc/lingshttputils/LingsHTTPUtils.java
@@ -5,15 +5,19 @@ import cn.lingsmc.lingshttputils.asyncworkers.WorkerOptions;
 import cn.lingsmc.lingshttputils.commands.Commands;
 import cn.lingsmc.lingshttputils.commands.TabComplete;
 import cn.lingsmc.lingshttputils.placeholderapi.PlaceholderAPI;
-import cn.lingsmc.lingshttputils.utils.YumBypassUtils;
 import com.google.common.collect.Maps;
 import lombok.Getter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import pw.yumc.Yum.config.FileConfig;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 
 /**
@@ -28,9 +32,13 @@ public final class LingsHTTPUtils extends JavaPlugin {
     @Getter
     private final Map<String, String> httpData = Maps.newConcurrentMap();
 
+    @Getter
+    private String pluginName;
+
     @Override
     public void onLoad() {
         instance = this;
+        pluginName = this.getPluginName();
         saveDefaultConfig();
         getConfig().options().copyDefaults(true);
     }
@@ -45,10 +53,23 @@ public final class LingsHTTPUtils extends JavaPlugin {
         command.setExecutor(new Commands());
         command.setTabCompleter(new TabComplete());
         // 绕过Yum网络监控
-        Bukkit.getPluginManager().registerEvents(new YumBypassUtils(), this);
+        Plugin yum = Bukkit.getPluginManager().getPlugin("Yum");
+        if (Objects.nonNull(yum)) {
+            final File file = new File(System.getProperty("user.dir") + "/plugins/Yum/network.yml");
+            FileConfig fileConfig = new FileConfig(file);
+            if(!fileConfig.getStringList("Ignore").contains(pluginName)){
+                fileConfig.set("Ignore",fileConfig.getStringList("Ignore").add(pluginName));
+                try {
+                    fileConfig.save(file);
+                } catch (IOException e) {
+                    getLogger().info("警告! Yum配置修改失败!");
+                }
+            }
+        }
         // bStats支持
         int pluginId = 16397;
         Metrics metrics = new Metrics(this, pluginId);
+        getLogger().info("bStats已连接!");
         // 运行Cycle worker
         WorkerOptions.runWorkers();
     }
