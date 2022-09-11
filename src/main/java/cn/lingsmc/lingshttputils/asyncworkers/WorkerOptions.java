bf14ca37dba1c8090ed4d579277f5d1038255701
泠辰 <2154818001@qq.com>
2022-09-11 16:32:01 +0800

    优化代码

--- src/main/java/cn/lingsmc/lingshttputils/asyncworkers/WorkerOptions.java
+++ src/main/java/cn/lingsmc/lingshttputils/asyncworkers/WorkerOptions.java
@@ -4,11 +4,10 @@ import cn.lingsmc.lingshttputils.LingsHTTPUtils;
 import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
+import java.util.logging.Level;
 
 /**
  * @author Crsuh2er0
@@ -18,12 +17,7 @@ import java.util.concurrent.ThreadFactory;
 public class WorkerOptions {
     @Getter
     static boolean started = true;
-    static ThreadFactory threadFactory = runnable -> {
-        Thread thread = new Thread(runnable);
-        thread.setDaemon(true);
-        return new Thread(runnable);
-    };
-    static List<Thread> threads = new ArrayList<>();
+    static ExecutorService threadPool;
 
 
     private WorkerOptions() {
@@ -34,40 +28,40 @@ public class WorkerOptions {
         FileConfiguration config = LingsHTTPUtils.config;
         Set<String> modules = config.getKeys(false);
         modules.remove("version");
+        modules.removeIf(module -> Objects.equals(config.getString(String.format("%s.reqMode", module)), "Cycle"));
+
+        threadPool = new ThreadPoolExecutor(1, modules.size(),
+                1L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(Integer.MAX_VALUE),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
 
         for (String module : modules) {
-            if (Objects.equals(config.getString(String.format("%s.reqMode", module)), "Cycle")) {
-                // 一定要为每个module设置一个worker
-                AsyncWorkers asyncWorkers = new AsyncWorkers();
-                final int reqTime = config.getInt(String.format("%s.reqTime", module));
-                final String url = config.getString(String.format("%s.url", module));
-                final String method;
-                final int refInterval = config.getInt(String.format("%s.refInterval", module));
-                if (Objects.nonNull(config.getString(String.format("%s.method", module)))) {
-                    method = config.getString(String.format("%s.method", module));
-                } else {
-                    method = "GET";
-                }
-                String[] keys = new String[0];
-                if ("json".equalsIgnoreCase(config.getString(String.format("%s.mode", module)))) {
-                    keys = config.getString(String.format("%s.key", module)).split("\\.");
-                }
-                String[] finalKeys = keys;
-                Runnable runnable = () -> asyncWorkers.asyncworker(module, reqTime, url, method, refInterval, finalKeys);
-                Thread t = threadFactory.newThread(runnable);
-                t.setName(String.format("LHU-%s", module));
-                LingsHTTPUtils.getInstance().getLogger().info(String.format("尝试启动%s...", t.getName()));
-                t.start();
-                threads.add(t);
+            // 一定要为每个module设置一个worker
+            AsyncWorkers asyncWorkers = new AsyncWorkers();
+            final int reqTime = config.getInt(String.format("%s.reqTime", module));
+            final String url = config.getString(String.format("%s.url", module));
+            final String method;
+            final int refInterval = config.getInt(String.format("%s.refInterval", module));
+            if (Objects.nonNull(config.getString(String.format("%s.method", module)))) {
+                method = config.getString(String.format("%s.method", module));
+            } else {
+                method = "GET";
             }
+            String[] keys = new String[0];
+            if ("json".equalsIgnoreCase(config.getString(String.format("%s.mode", module)))) {
+                keys = config.getString(String.format("%s.key", module)).split("\\.");
+            }
+            String[] finalKeys = keys;
+            Runnable runnable = () -> asyncWorkers.asyncworker(module, reqTime, url, method, refInterval, finalKeys);
+            LingsHTTPUtils.getInstance().getLogger().log(Level.SEVERE, "尝试启动{0}...", module);
+            threadPool.execute(runnable);
         }
     }
 
     public static void stopWorkers() {
         LingsHTTPUtils.getInstance().getLogger().info("尝试关闭Cycle Worker...");
-        for(Thread t : threads){
-            t.stop();
-        }
+        threadPool.shutdownNow();
         started = false;
     }
 }
