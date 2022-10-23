61501cc637787d70b550b8145ac5a54a43e792f7
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2022-10-23 08:17:25 +0800

    修复高版本Json解析失败的bug

--- src/main/java/cn/lingsmc/lingshttputils/utils/JsonUtils.java
+++ src/main/java/cn/lingsmc/lingshttputils/utils/JsonUtils.java
@@ -71,9 +71,9 @@ public class JsonUtils {
      */
     public static String getValueGson(String str, String @NotNull [] keys) {
         JsonObject value = parseStrGson(str);
-        for (String key : keys) {
-            value = value.get(key).getAsJsonObject();
+        for (int i = 0;i < keys.length - 1;i++) {
+            value = value.get(keys[i]).getAsJsonObject();
         }
-        return value.toString();
+        return value.get(keys[keys.length - 1]).getAsString();
     }
 }
