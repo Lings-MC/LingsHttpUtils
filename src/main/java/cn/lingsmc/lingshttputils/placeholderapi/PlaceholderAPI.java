39b47f63534c3f992ea187c6c8f2b51ae7d71546
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2022-10-23 08:24:55 +0800

    修复enabled检测

--- src/main/java/cn/lingsmc/lingshttputils/placeholderapi/PlaceholderAPI.java
+++ src/main/java/cn/lingsmc/lingshttputils/placeholderapi/PlaceholderAPI.java
@@ -52,6 +52,9 @@ public class PlaceholderAPI extends PlaceholderExpansion {
     public String onRequest(OfflinePlayer p, @NotNull String params) {
         Set<String> modules = config.getKeys(false);
         modules.removeIf(module -> Objects.equals(config.getBoolean(String.format("%s.enabled", module)), false));
+        if(modules.isEmpty()){
+            return null;
+        }
         for (String module : modules) {
             if (Objects.equals(params, config.getString(String.format("%s.apiname", module)))) {
                 if ("inTime".equalsIgnoreCase(config.getString(String.format("%s.reqMode", module)))) {
