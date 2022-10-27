805acc362213fa34d9817315be1cd1eb8f56ee8e
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2022-10-27 11:27:28 +0800

    request指令

---
+++ src/main/java/cn/lingsmc/lingshttputils/commands/RequestCommand.java
@@ -0,0 +1,41 @@
+package cn.lingsmc.lingshttputils.commands;
+
+import cn.lingsmc.lingshttputils.LingsHttpUtils;
+import cn.lingsmc.lingshttputils.utils.MessageUtils;
+import cn.lingsmc.lingshttputils.utils.RequestUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author Crsuh2er0
+ * @apiNote
+ * @since 2022/10/20
+ */
+public class RequestCommand {
+    private RequestCommand() {
+    }
+    private static final FileConfiguration CONFIG = LingsHttpUtils.getInstance().getConfig();
+    static LingsHttpUtils plugin = LingsHttpUtils.getInstance();
+
+    public static void request(String module, @NotNull CommandSender sender){
+        if (!sender.hasPermission(Commands.permission)) {
+            sender.sendMessage(String.format("%s你没有执行此命令的权限.", ChatColor.RED));
+            return;
+        }
+        // 判断是否有此模块
+        Set<String> modules = CONFIG.getKeys(false);
+        if (!modules.contains(module)){
+            MessageUtils.sendMessage(sender,String.format("%s错误:未找到此模块. 请检查拼写以及是否已重载配置文件.",ChatColor.RED));
+            return;
+        }
+        // 不管是循环还是及时都直接请求一次
+        String res = RequestUtils.request(module, CONFIG, plugin);
+        // 判断结果是否为null
+        MessageUtils.sendMessage(sender,String.format("%s%s %s获取到的结果为 %s%s",ChatColor.AQUA,module,ChatColor.GREEN,ChatColor.AQUA,Objects.isNull(res) ? "null" : res));
+    }
+}
