8d6d64e05d4bcd65a352517fdeb19773607b764b
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2023-01-30 18:42:44 +0800

    修正命令提示

--- src/main/java/cn/lingsmc/lingshttputils/commands/Commands.java
+++ src/main/java/cn/lingsmc/lingshttputils/commands/Commands.java
@@ -13,7 +13,7 @@ import org.jetbrains.annotations.NotNull;
  * @apiNote
  */
 public class Commands implements CommandExecutor {
-    static String helpMessage = String.format("%s未知命令!输入/lhu help查看命令列表!", ChatColor.RED);
+    static String helpMessage = String.format("%s未知命令! 输入/lhu help查看命令列表!", ChatColor.RED);
     static String rootMessage1 = String.format("%s此服务器正在运行 %s%s %s%s by %s", ChatColor.DARK_AQUA, ChatColor.AQUA, LingsHttpUtils.getInstance().getName(), LingsHttpUtils.getInstance().getDescription().getVersion(), ChatColor.DARK_AQUA, "§aC§br§cs§du§eh§a2§be§cr§d0");
     static String rootMessage2 = String.format("%s命令列表: %s/lhu help", ChatColor.DARK_AQUA, ChatColor.AQUA);
     static String permission = "lingshttputils.admin";
